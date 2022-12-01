e7631090ce8ad5639524750bba89569f12c5cb21
Yash890-star <81615944+Yash890-star@users.noreply.github.com>
2022-12-01 19:30:50 +0000

    done

--- app.py
+++ app.py
@@ -24,39 +24,31 @@ def predict():
 	global nut
 	global out
 	if request.method == "POST":
-
-		# statw
-		state_no = int(request.form['state'])
-		state = tools.state_id(state_no)
-		#print("state= ",state)
-		# city
-		city = str(request.form['city'])
+		print('this works')
+		# district
+		dis = str(request.form.get('dis'))
+		print(dis)
 		# nitrogen ratio
-		N = float(request.form['N'])
+		N = float(request.form.get('N'))
 		# potassium ratio
-		K = float(request.form['K'])
+		K = float(request.form.get('K'))
 		# phospours ratio
-		P = float(request.form['P'])
+		P = float(request.form.get('P'))
         # ph
-		ph = float(request.form['ph'])
-		
+		ph = float(request.form.get('ph'))
+		print(dis, N, K, P, ph)
 		npk = [N,P,K,ph]
-		#print(npk)
-		nut = [calcium,magnesium ,sulphur,zinc,iron,boron,magnese]
-		unit =[ca_unit,mg_unit,s_unit,zn_unit,fe_unit,b_unit,mn_unit]
-		#print(npk,nut,unit)
-		nut = tools.convert(nut,unit)
 		#print(nut)
 		#append temp,humidity,rainfall in input_lst
-		loc=tools.loc_att(state,city)
-		input_lst=[npk[0],npk[1],npk[2],loc[0],loc[1],npk[3],loc[2]]
-		input_lst = np.array(input_lst).reshape(1,-1)
-		pred = model.predict(input_lst)
-		out = pred[0]
+		#loc=tools.loc_att(dis)
+		#input_lst=[npk[0],npk[1],npk[2],loc[0],loc[1],npk[3],loc[2]]
+		#input_lst = np.array(input_lst).reshape(1,-1)
+		#pred = model.predict(input_lst)
+		#pyout = pred[0]
 		#print(out)
 		
 
-		return render_template("{}.html".format(out))
+		return render_template("predict.html".format(out))
 	
 		
 
